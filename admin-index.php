851585de65a96939334b3a8ee2992192d4e9ddee
WaKeMaTTa <m.ziata@hotmail.com>
2014-02-17 15:01:45 +0100

    Fix Textdomain

--- admin-index.php
+++ admin-index.php
@@ -15,19 +15,19 @@ if ( !function_exists( 'add_action' ) ) {
 
 	<div class="icon32 icon32-main"><br></div>
 
-	<h2><?php _e( 'WP RuneQuesters', WP_ROLE_TEXTDOMAIN ); ?></h2>
+	<h2><?php _e( 'WP RuneQuesters', WPRQ_TEXTDOMAIN ); ?></h2>
 
 	<div id="welcome-panel" class="welcome-panel">
 
 		<div class="welcome-panel-content">
 
-			<h3><?php echo sprintf( __( 'Welcome to %s', WP_ROLE_TEXTDOMAIN ), __( 'WP RuneQuesters', WP_ROLE_TEXTDOMAIN ) ); ?></h3>
+			<h3><?php echo sprintf( __( 'Welcome to %s', WPRQ_TEXTDOMAIN ), __( 'WP RuneQuesters', WPRQ_TEXTDOMAIN ) ); ?></h3>
 
-			<p class="about-description"><?php _e( 'Right now you are in the beginning of your adventures.', WP_ROLE_TEXTDOMAIN ); ?></p>
+			<p class="about-description"><?php _e( 'Right now you are in the beginning of your adventures.', WPRQ_TEXTDOMAIN ); ?></p>
 
 			<ul class="actions-list">
 
-				<!--li><a href="" class="action-icon action-add-character"><?php _e( 'Create your character', WP_ROLE_TEXTDOMAIN ); ?></a></li-->
+				<!--li><a href="" class="action-icon action-add-character"><?php _e( 'Create your character', WPRQ_TEXTDOMAIN ); ?></a></li-->
 
 				<ul class="todo">
 
@@ -39,9 +39,9 @@ if ( !function_exists( 'add_action' ) ) {
 
 							<div class="todo-content">
 
-								<h4 class="todo-name"><?php _e( 'My characters', WP_ROLE_TEXTDOMAIN ); ?></h4>
+								<h4 class="todo-name"><?php _e( 'My characters', WPRQ_TEXTDOMAIN ); ?></h4>
 
-								<?php _e( 'Giving life to an entity', WP_ROLE_TEXTDOMAIN ); ?>
+								<?php _e( 'Giving life to an entity', WPRQ_TEXTDOMAIN ); ?>
 
 							</div>
 
@@ -57,9 +57,9 @@ if ( !function_exists( 'add_action' ) ) {
 
 							<div class="todo-content">
 
-								<h4 class="todo-name"><?php _e( 'The World', WP_ROLE_TEXTDOMAIN ); ?></h4>
+								<h4 class="todo-name"><?php _e( 'The World', WPRQ_TEXTDOMAIN ); ?></h4>
 
-								<?php _e( 'The interactive maps', WP_ROLE_TEXTDOMAIN ); ?>
+								<?php _e( 'The interactive maps', WPRQ_TEXTDOMAIN ); ?>
 
 							</div>
 
